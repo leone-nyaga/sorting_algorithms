be53badf490a3a99e956d97a2c2d49472ba545d7
leone-nyaga <leonenyaga@gmail.com>
2023-09-26 09:37:54 -0700

    Betty

--- print_array.c
+++ print_array.c
@@ -8,14 +8,13 @@
  */
 void print_array(const int *array, size_t size)
 {
-    size_t i;
+	size_t i;
 
-    for (i = 0; i < size; i++)
-    {
-        printf("%d", array[i]);
-        if (i < size - 1)
-            printf(", ");
-    }
-    printf("\n");
+	for (i = 0; i < size; i++)
+	{
+		printf("%d", array[i]);
+		if (i < size - 1)
+			printf(", ");
+	}
+	printf("\n");
 }
-
